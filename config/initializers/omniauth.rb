91a24858cfc2cd56f974b470e711a06045466374
Kris <andagi@gmail.com>
2013-04-16 18:47:56 -0700

    Added Facebook Stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 end
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+	provider :facebook, '295385383926334', 'a4a145c82db9a7c5b2f379e8c0c7be8b'
 end
